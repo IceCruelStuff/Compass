c0f43e4e5d3b0bfb0d50440e97219b0dce4caf11
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-03-30 18:04:42 -0700

    Update ShowBarTask.php

--- src/Compass/ShowBarTask.php
+++ src/Compass/ShowBarTask.php
@@ -9,32 +9,32 @@ use pocketmine\scheduler\Task;
 
 class ShowBarTask extends Task{
 	
-    /** @var CompassBar */
-    private $plugin;
+	/** @var CompassBar */
+	private $plugin;
 	
-    public function __construct(CompassBar $plugin){
-        $this->plugin = $plugin;
-    }
+	public function __construct(CompassBar $plugin){
+		$this->plugin = $plugin;
+	}
 	
-    public function onRun(int $currentTick){
-        $bossBar = $this->plugin->getBossBar();
-        /** @var Player $viewer */
-        foreach($bossBar->getViewers() as $viewer){
-            if(!$viewer->isClosed()){
-                $bossBar->setTitle(Utils::getCompass($viewer->yaw));
-                $bossBar->updateFor($viewer);
-            }else{
-                $viewer->removeBossbar(CompassBar::BOSSBAR_ID);
-            }
-        }
-    }
+	public function onRun(int $currentTick){
+		$bossBar = $this->plugin->getBossBar();
+		/** @var Player $viewer */
+		foreach($bossBar->getViewers() as $viewer){
+			if(!$viewer->isClosed()){
+				$bossBar->setTitle(Utils::getCompass($viewer->yaw));
+				$bossBar->updateFor($viewer);
+			} else {
+				$viewer->removeBossbar(CompassBar::BOSSBAR_ID);
+			}
+		}
+	}
 	
-    public function onCancel(){
-        $bossBar = $this->plugin->getBossBar();
+	public function onCancel(){
+		$bossBar = $this->plugin->getBossBar();
 		
-        /** @var Player $viewer */
-        foreach($bossBar->getViewers() as $viewer){
-            $viewer->removeBossbar(CompassBar::BOSSBAR_ID);
-        }
-    }
+		/** @var Player $viewer */
+		foreach($bossBar->getViewers() as $viewer){
+			$viewer->removeBossbar(CompassBar::BOSSBAR_ID);
+		}
+	}
 }
